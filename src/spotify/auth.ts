036d5172216255038f2e863b4b68dcf62363b16d
Lukas Tetzlaff <lukastetzlaff@gmail.com>
2018-07-14 11:51:40 +0200

    :rocket: :lock: add oauth wrapper for spotify auth

---
+++ src/spotify/auth.ts
@@ -0,0 +1,16 @@
+import Routes from "./routes"
+import OAuth2 from "improved/dist/oauth2"
+import authCache from "../utils/auth-cache"
+import { SPOTIFY_CLIENT_ID, SPOTIFY_SECRET, SPOTIFY_REDIRECT_URI } from "../utils/envs"
+
+const config = {
+  clientID: SPOTIFY_CLIENT_ID,
+  clientSecret: SPOTIFY_SECRET,
+  redirectURI: SPOTIFY_REDIRECT_URI,
+  tokenURL: Routes.token,
+  authorizeURL: Routes.authorize
+}
+
+const oauth = (async () => new OAuth2(config, await authCache.read()))()
+
+export default oauth
