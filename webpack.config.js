3686bb1c8285830de1e2d52ab97042cde9d98c0c
Lukas Tetzlaff <lukastetzlaff@gmail.com>
2018-07-16 01:01:25 +0200

    :wrench: change compiler settings

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require("path")
 const slsw = require("serverless-webpack")
 
 const entries = {}
-
 Object.keys(slsw.lib.entries).forEach(
   key => (entries[key] = ["./source-map-install.js", slsw.lib.entries[key]])
 )
@@ -12,17 +11,15 @@ module.exports = {
   entry: entries,
   devtool: "source-map",
   resolve: {
-    extensions: [".js", ".jsx", ".json", ".ts", ".tsx"],
+    extensions: [".js", ".jsx", ".json", ".ts", ".tsx"]
   },
   output: {
     libraryTarget: "commonjs",
     path: path.join(__dirname, ".webpack"),
-    filename: "[name].js",
+    filename: "[name].js"
   },
   target: "node",
   module: {
-    rules: [
-      { test: /\.tsx?$/, loader: "ts-loader" },
-    ],
-  },
+    rules: [{ test: /\.tsx?$/, loader: "ts-loader" }]
+  }
 }
