8069fd1d4b41b990227e95bfc3d1183d29775632
Lukas Tetzlaff <lukastetzlaff@gmail.com>
2018-08-06 00:01:35 +0200

    :hammer: add new routes to deployment

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,7 @@ import hello from "./handlers/hello"
 import redirect from "./handlers/redirect"
 import login from "./handlers/login"
 import api from "./handlers/api"
+import room from "./handlers/room"
+import join from "./handlers/join"
 
-export { hello, api, login, redirect }
+export { hello, api, login, redirect, room, join }
